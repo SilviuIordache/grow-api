a3eb65dc32f54b1b9d49a40a9336b542925d2304
Silviu Iordache <silviu.iordache@caphyon.com>
2021-06-01 21:21:36 +0300

    Save evaluation ids in the user document.

--- controllers/evaluationController.js
+++ controllers/evaluationController.js
@@ -1,10 +1,17 @@
 const Evaluation = require("../models/evaluationModel");
 const catchAsync = require("./../utils/catchAsync.js");
+const User = require("./../models/userModel");
 
 exports.createEvaluation = catchAsync(async (req, res, next) => {
-  console.log(req.user);
-  
   const evaluation = await Evaluation.create(req.body);
+
+  //save evaluation id in user's evaluations array
+  await User.findByIdAndUpdate(req.user._id, {
+    $addToSet: {
+      evaluations: evaluation._id,
+    },
+  });
+
   res.status(201).json({
     status: "success",
     data: {
